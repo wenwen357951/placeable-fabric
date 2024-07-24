902604e8b18bdc5df5d12b2f3adba8de421e837b
SHENGWEN CHEN <wenwen357951@gmail.com>
2024-07-24 17:31:14 +0800

    feat: Add new plants Small Dripleaf can be placed anywhere

---
+++ src/main/java/it/bisumto/placeable/mixin/SmallDripleafBlockMixin.java
@@ -0,0 +1,23 @@
+package it.bisumto.placeable.mixin;
+
+import it.bisumto.placeable.Placeable;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SmallDripleafBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SmallDripleafBlock.class)
+public class SmallDripleafBlockMixin {
+
+    // PLACEABLE
+    @Inject(method = "canPlaceAt", at = @At("HEAD"), cancellable = true)
+    public void canPlantAnywhere(BlockState blockState, WorldView world, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
+        if (Placeable.isValidFloor(world, blockPos)) {
+            cir.setReturnValue(true);
+        }
+    }
+}
