6239b7044734148814a23a22ce011e47607873c1
Wayne (Chen ShengWen) <wenwen357951@gmail.com>
2025-04-21 17:26:29 +0800

    feat: Update to mc1.21.5
    refactor: Update 1.21.5 JSON structure for large fern and tall grass
    refactor: Rename DeadBushBlockMixin to DryVegetationBlockMixin
    chore: Update Minecraft and Fabric dependencies
    chore: Update Gradle wrapper to version 8.12

--- src/main/java/com/wennest/placeable/mixin/DryVegetationBlockMixin.java
+++ src/main/java/com/wennest/placeable/mixin/DryVegetationBlockMixin.java
@@ -3,7 +3,7 @@ package com.wennest.placeable.mixin;
 import com.wennest.placeable.Placeable;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.DeadBushBlock;
+import net.minecraft.block.DryVegetationBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,8 +11,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(DeadBushBlock.class)
-public class DeadBushBlockMixin {
+@Mixin(DryVegetationBlock.class)
+public class DryVegetationBlockMixin {
 
     // PLACEABLE
     @Inject(method = "canPlantOnTop", at = @At("HEAD"), cancellable = true)
